b13afd53149b0a2a1ce277dad74ca18f34425075
igurkina <yugurkina@gmail.com>
2017-04-22 21:20:04 +0200

    Added excercise #4

---
+++ src/01_04.java
@@ -0,0 +1,11 @@
+/*
+ * Write a program that prints the smallest and largest positive double value.
+ */
+
+class Border_Doubles {
+	
+	public static void main(String[] args) {
+		System.out.println("The largest positive double: " + Double.MAX_VALUE);
+		System.out.println("The smallest positive double: " + Double.MIN_VALUE);
+	}
+}
